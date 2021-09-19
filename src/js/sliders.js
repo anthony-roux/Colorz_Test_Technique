c1be1c4b98794fdcd53be230affa860b3e51e2ec
[anthony.roux] <[anthony.roux@doctolib.com]>
2021-09-19 23:00:35 +0200

    clean

--- src/js/sliders.js
+++ src/js/sliders.js
@@ -1,20 +1,4 @@
-
-
-console.log("coucou");
-console.log("crotte");
-
-
-// var myCarousel = document.querySelector('#myCarousel')
-// var carousel = new bootstrap.Carousel(myCarousel)
-
-// var slider1 = document.querySelector('#slider1');
-// var carousel = new bootstrap.Carousel(slider1);
-
-// $(document).ready(function(){
-//   $('#slider1').carousel({
-//     interval: 3000
-//   })
-// });  
+ 
 
 jQuery(document).ready(function ($) {
 
